c0fd1b53a9df9e82d4f6cbb3e1a8eebb690c741a
Ma Dongge <dongge.23@intl.zju.edu.cn>
2024-03-06 15:47:24 +0800

    add Practical4

---
+++ Practical4/recursive.py
@@ -0,0 +1,4 @@
+a = 4  #Assign an initial value
+for i in range(5):  #cycle 5 times
+    print(a)    #print the outcomes
+    a = 2*a +3    #update its value and cycle
---
+++ Practical4/variables.py
@@ -0,0 +1,19 @@
+a=40 #just use the time to simplify
+b=36
+c=30
+d=a-b
+e=b-c
+if d>=e :
+    print('only training makes great improvement')
+else:print('strength training makes great improvement')
+X=True
+Y=False
+W='either X or Y'
+
+#truth table
+# X      Y      W
+# True   True   True
+# True   False  True
+# False  True   True  
+# False   False False
+#

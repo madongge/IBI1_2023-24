c7276be9e2c3660f98c60a2054476e06eacfff8c
Ma Dongge <dongge.23@intl.zju.edu.cn>
2024-04-10 15:06:37 +0800

    update

--- Practical9/IBI1_student_database.py
+++ Practical9/IBI1_student_database.py
@@ -1,14 +1,14 @@
 class Student:
-    def __init__(self, name, major, code_portfolio_score, group_project_score, exam_score):
-        self.name = name
-        self.major = major
-        self.code_portfolio_score = code_portfolio_score
-        self.group_project_score = group_project_score
-        self.exam_score = exam_score
+    def __init__(students, name, major, code_portfolio_score, group_project_score, exam_score):
+        students.name = name
+        students.major = major
+        students.code_portfolio_score = code_portfolio_score
+        students.group_project_score = group_project_score
+        students.exam_score = exam_score
 
-    def print_details(self):
+    def print_details(students):
         print('Name: {}, Major: {}, Code Portfolio Score: {}, Group Project Score: {}, Exam Score: {}'.format(
-            self.name, self.major, self.code_portfolio_score, self.group_project_score, self.exam_score))
+            students.name, students.major, students.code_portfolio_score, students.group_project_score, students.exam_score))
 
 # Example
 student1 = Student('Ma Dongge', 'BMI', 85, 90, 80)

bae5eb345941469cbedb633fd8c75ab49863f0f9
Ma Dongge <dongge.23@intl.zju.edu.cn>
2024-04-10 14:48:33 +0800

    practical9

--- Practical9/Chocolate_bar_affordability_calculator.py
+++ Practical9/Chocolate_bar_affordability_calculator.py
@@ -7,4 +7,4 @@ def calculate_chocolate_bars(total_money, price_per_bar):
 total_money = 100
 price_per_bar = 7
 bars, change = calculate_chocolate_bars(total_money, price_per_bar)
-print(f"With {total_money} money, you can buy {bars} chocolate bars and have {change} left over.")
+print(f'With {total_money} money, you can buy {bars} chocolate bars and have {change} left over.')

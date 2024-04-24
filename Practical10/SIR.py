71178bf4984a3ee03d6de53685db609a8bcff5c5
Ma Dongge <dongge.23@intl.zju.edu.cn>
2024-04-24 16:03:52 +0800

    update

---
+++ Practical10/SIR.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 10000
+I = 1
+S = N - I
+R = 0
+beta = 0.3
+gamma = 0.05
+
+susceptible = [S]
+infected = [I]
+recovered = [R]
+
+for _ in range(1000):
+    # 计算每个时间点的概率
+    prob_infection = beta * (I / N)
+    prob_recovery = gamma
+    
+    # 感染者和易感染者的人数
+    newly_infected = np.random.choice([0, 1], S, p=[1 - prob_infection, prob_infection]).sum()
+    newly_recovered = np.random.choice([0, 1], I, p=[1 - prob_recovery, prob_recovery]).sum()
+    
+    S -= newly_infected
+    I += newly_infected - newly_recovered
+    R += newly_recovered
+    
+    susceptible.append(S)
+    infected.append(I)
+    recovered.append(R)
+
+plt.figure(figsize=(6, 4), dpi=150)
+plt.plot(susceptible, label='Susceptible')
+plt.plot(infected, label='Infected')
+plt.plot(recovered, label='Recovered')
+plt.xlabel('Time')
+plt.ylabel('Population')
+plt.title('SIR Model')
+plt.legend()
+
+save_path = "/Users/madongge/Downloads/IBI1/IBI1_2023-24/Practical10/SIR_Model_Plot.png"
+plt.savefig(save_path)
+plt.show()
---
+++ Practical10/spatial_SIR.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+beta = 0.3
+gamma = 0.05
+#100x100 的数组
+population = np.zeros((100, 100))
+
+outbreak = np.random.choice(range(100), 2)
+population[outbreak[0], outbreak[1]] = 1  
+
+for t in range(100):
+    new_population = population.copy()
+    
+    for i in range(100):
+        for j in range(100):
+            if population[i, j] == 1:  
+                neighbors = [(i-1, j-1), (i-1, j), (i-1, j+1),
+                             (i, j-1),             (i, j+1),
+                             (i+1, j-1), (i+1, j), (i+1, j+1)]
+                for ni, nj in neighbors:
+                    if 0 <= ni < 100 and 0 <= nj < 100:  
+                        if population[ni, nj] == 0:  
+                            if np.random.random() < beta:
+                                new_population[ni, nj] = 1  
+                if np.random.random() < gamma:
+                    new_population[i, j] = 2  
+
+    population = new_population
+
+    plt.figure(figsize=(6, 4), dpi=150)
+    plt.imshow(population, cmap='viridis', interpolation='nearest')
+    plt.title(f'Time Point: {t}')
+    plt.show()
